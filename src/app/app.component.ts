9e4170d7ca7b1737d22f40cefb0259f4cd31e35f
wecraw <will@ccrawford.org>
2025-01-19 18:33:57 -0800

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { MarqueeComponent } from './marquee/marquee.component';
 import { CommonModule } from '@angular/common';
 import { NgTiltModule } from '@geometricpanda/angular-tilt';
+import { chefNames } from './chefNames';
 
 @Component({
   selector: 'app-root',
@@ -12,44 +13,13 @@ import { NgTiltModule } from '@geometricpanda/angular-tilt';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  chefNames = chefNames;
   title = 'chef-frontend';
 
-  chefNames = [
-    'Creative Hues Erupting Funk',
-    'Chromatic Heat Energy Fusion',
-    'Celestial Groove Evolutionary Force',
-    'Chicago Hotdog, Extra Fries',
-    'Conscious Harmonic Energy Flow',
-    'Captivating Harmonic Eclectic Funk',
-    'Cool Heat Embracing Funk',
-    'Coupla Homies Emitting Frequencies',
-    'Crazy Harmonic Euphoric Funk',
-    'Creative Hues Erupting Funk',
-    'Chromatic Heat Energy Fusion',
-    'Celestial Groove Evolutionary Force',
-    'Chicago Hotdog, Extra Fries',
-    'Conscious Harmonic Energy Flow',
-    'Captivating Harmonic Eclectic Funk',
-    'Cool Heat Embracing Funk',
-    'Coupla Homies Emitting Frequencies',
-    'Crazy Harmonic Euphoric Funk',
-    'Creative Hues Erupting Funk',
-    'Chromatic Heat Energy Fusion',
-    'Celestial Groove Evolutionary Force',
-    'Chicago Hotdog, Extra Fries',
-    'Conscious Harmonic Energy Flow',
-    'Captivating Harmonic Eclectic Funk',
-    'Cool Heat Embracing Funk',
-    'Coupla Homies Emitting Frequencies',
-    'Crazy Harmonic Euphoric Funk',
-    'Creative Hues Erupting Funk',
-    'Chromatic Heat Energy Fusion',
-    'Celestial Groove Evolutionary Force',
-    'Chicago Hotdog, Extra Fries',
-    'Conscious Harmonic Energy Flow',
-    'Captivating Harmonic Eclectic Funk',
-    'Cool Heat Embracing Funk',
-    'Coupla Homies Emitting Frequencies',
-    'Crazy Harmonic Euphoric Funk',
-  ];
+  scrollToSection(sectionId: string) {
+    const element = document.querySelector('#' + sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
 }
