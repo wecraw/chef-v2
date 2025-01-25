42a41b88e9653964eb92600b56152e9b57e53e74
wecraw <will@ccrawford.org>
2025-01-25 11:56:48 -0800

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MarqueeComponent } from './marquee/marquee.component';
 import { CommonModule } from '@angular/common';
@@ -15,11 +15,23 @@ import { chefNames } from './chefNames';
 export class AppComponent {
   chefNames = chefNames;
   title = 'chef-frontend';
+  showHeader = false;
+  firstLoad = true;
 
   scrollToSection(sectionId: string) {
     const element = document.querySelector('#' + sectionId);
     if (element) {
       element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    const musicSection = document.getElementById('music-header');
+    if (musicSection) {
+      const musicSectionRect = musicSection.getBoundingClientRect();
+      this.showHeader = window.scrollY > musicSectionRect.bottom;
+      if (this.showHeader) this.firstLoad = false;
     }
   }
 }
